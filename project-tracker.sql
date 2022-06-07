9a0726dc2292b107d76bef69890ab1cac35711f5
Rachel Moon <hj.rachelmoon@gmail.com>
2022-06-07 12:03:53 -0700

    Completed Lab

--- project-tracker.sql
+++ project-tracker.sql
@@ -13,12 +13,48 @@ SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
-SET client_min_messages =, warning;
+SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: grades; Type: TABLE; Schema: public; Owner: rachelmoon
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO rachelmoon;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: rachelmoon
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO rachelmoon;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rachelmoon
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
 
 --
 -- Name: projects; Type: TABLE; Schema: public; Owner: rachelmoon
@@ -93,6 +129,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: rachelmoon
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: rachelmoon
 --
 
@@ -104,6 +147,18 @@ ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.pro
 --
 
 ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: rachelmoon
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+\.
 
 
 --
@@ -130,6 +185,13 @@ COPY public.students (id, first_name, last_name, github) FROM stdin;
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: rachelmoon
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 4, true);
+
+
+--
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: rachelmoon
 --
 
@@ -141,6 +203,14 @@ SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
 --
 
 SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: rachelmoon
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
